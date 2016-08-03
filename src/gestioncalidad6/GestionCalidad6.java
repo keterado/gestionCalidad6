5573fef5e94245c9855c2ed07195a0c642b5aeba
Usuario <Usuario@Pc>
2016-08-03 13:41:03 -0500

    primera subida

---
+++ src/gestioncalidad6/GestionCalidad6.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gestioncalidad6;
+
+/**
+ *
+ * @author Usuario
+ */
+public class GestionCalidad6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        menuPadre menuInicio = new menuPadre();
+        menuInicio.setTitle("Menú de inicio");
+        menuInicio.setVisible(true);
+    }
+    
+}
